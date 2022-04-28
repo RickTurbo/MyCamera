6cd9e9b6cd71186394800bb241a06577a37fcc79
kojima rikuto <kojikoji7765@icloud.com>
2022-04-28 18:35:41 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 児島　陸斗 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
