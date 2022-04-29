a09d558624a10b109f9d088a13d02c2e26934c7d
kojima rikuto <kojikoji7765@icloud.com>
2022-04-29 21:50:36 +0900

    撮影機能やsns共有機能の追加

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet = false
+    @State var isShowActivity = false
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            Text("写真アプリ")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+            Spacer()
+
+            if let unwrapCaptureImage = captureImage {
+                Image(uiImage: unwrapCaptureImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+
+            Spacer()
+
+            Button(action: {
+                if UIImagePickerController.isSourceTypeAvailable(.camera)
+                {
+                    print("カメラは利用できます")
+                    isShowSheet = true
+                } else {
+                    print("カメラは利用できません")
+                }
+            }) {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+
+            }
             .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(
+                    isShowSheet: $isShowSheet,
+                    captureImage: $captureImage
+                )
+            }
+
+            Button(action: {
+                if let _ = captureImage {
+                    isShowActivity = true
+                }
+            }) {
+                Text("SNSに投稿する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+
+            }
+            .padding()
+            .sheet(isPresented: $isShowActivity) {
+                ActivityView(shareItems: [captureImage!])
+            }
+        }
     }
 }
 
---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,60 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 児島　陸斗 on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+
+    @Binding var isShowSheet: Bool
+    @Binding var captureImage: UIImage?
+
+    class Coordinator: NSObject,
+                       UINavigationControllerDelegate,
+                       UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info:
+            [UIImagePickerController.InfoKey : Any]) {
+                if let originalImage =
+                    info[UIImagePickerController.InfoKey.originalImage]
+                    as? UIImage {
+                    parent.captureImage = originalImage
+                }
+
+                parent.isShowSheet = false
+            }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isShowSheet = false
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(
+        context: UIViewControllerRepresentableContext<ImagePickerView>) ->
+    UIImagePickerController {
+        let myImagePickerController = UIImagePickerController()
+        myImagePickerController.sourceType = .camera
+        myImagePickerController.delegate = context.coordinator
+
+        return myImagePickerController
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+
+    }
+}
+
